773eeee3eabbf0f9690e69a68841a3dead0454ab
ra2411 <35757161+ra2411@users.noreply.github.com>
2018-01-24 01:35:25 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var vid = document.getElementById("video");
+
+function display() {
+	if(vid.style.display === "block") {
+		vid.style.display = "none";
+	}
+	else {
+		vid.style.display = "block";
+	}
+}
+
+
+function playVid() {
+	vid.play();
+}
+
+function pauseVid() {
+	vid.pause();
+}
+
+function video() {
+	setTimeout(playVid, 5000);
+}
